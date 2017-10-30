c998cc9c081e3b9e5073d2a036fb30d776914049
Your Name <you@example.com>
2017-10-30 21:42:56 +0100

    Adding config_files

---
+++ config_files/owncloud/config.php
@@ -0,0 +1,24 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocd2r55g2tfx',
+  'passwordsalt' => 'L/Qo348TS6WWolGNiGJGNYSWRH32tB',
+  'secret' => 'BDsy110xkaKnNtWtqNXbiQre/ll03hRFJiBWaht0yiyLpTtc',
+  'trusted_domains' => 
+  array (
+    0 => '172.18.0.5',
+    1 => '127.0.0.1',
+    1 => '127.0.0.1:8081',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'https://172.18.0.5',
+  'dbtype' => 'mysql',
+  'version' => '10.0.3.3',
+  'dbname' => 'oc_db1',
+  'dbhost' => 'mysql-oc',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc',
+  'dbpassword' => 'oc',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  "overwriteprotocol" => "https",
+);
